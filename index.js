15439613242e80e934faa186367a17db048a81c6
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-02-27 20:27:42 +0000

    created instances and separated questions using while loop

--- index.js
+++ index.js
@@ -10,79 +10,108 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+const teamMembers = [];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+async function questions() {
+  // ask questions to get manager information
+  const { name, id, email, officeNumber } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What's the team manager's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What's the team manager's employee ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What's the team manager's email?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What's the team manager's office number?",
+    },
+  ]);
 
+  // create a new Manager instance and add it to the teamMembers array
+  const manager = new Manager(name, id, email, officeNumber);
+  teamMembers.push(manager);
 
-const questions = await inquirer.prompt([
-    {
-        type: 'input',
-        name: 'Title',
-        message: "What's the team manager's name?",
-    },
-    {
-        type: 'input',
-        name: 'ID',
-        message: "What's the team manager's employee ID?",
-    },
-    {
-        type: 'input',
-        name: 'Email',
-        message: "What's the team manager's email?",
-    },
-    {
-        type: 'input',
-        name: 'Office Number',
-        message: "What's the team manager's office number?",
-    },
-    {
-        type: 'list',
-        name: 'Add staff members',
-        message: "Would you like to add any other members of staff?",
-        choices: ['Add an engineer', 'Add an intern', 'Finish building the team']
-    },
-    {
-        type: 'input',
-        name: 'Title',
-        message: "What's the engineer's name?",
-    },
-    {
-        type: 'input',
-        name: 'ID',
-        message: "What's the engineer's employee ID?",
-    },
-    {
-        type: 'input',
-        name: 'Email',
-        message: "What's the engineer's email?",
-    },
-    {
-        type: 'input',
-        name: 'GitHub',
-        message: "What's the engineer's GitHub username?",
-    },
-    {
-        type: 'input',
-        name: 'Title',
-        message: "What's the intern's name?",
-    },
-    {
-        type: 'input',
-        name: 'ID',
-        message: "What's the intern's employee ID?",
-    },
-    {
-        type: 'input',
-        name: 'Email',
-        message: "What's the intern's email?",
-    },
-    {
-        type: 'input',
-        name: 'School',
-        message: "What's the intern's school called?",
-    }
-]);
+  let addMore = true;
 
-const employees = [Engineer, Intern, Manager];
-const html = render(employees);
-fs.writeFileSync(outputPath, html);
+  while (addMore) {
+    const { employeeType } = await inquirer.prompt({
+      type: "list",
+      name: "employeeType",
+      message: "Which type of employee would you like to add next?",
+      choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+    });
+};
+    switch (employeeType) {
+      case "Engineer":
+        const { engName, engId, engEmail, github } = await inquirer.prompt([
+          {
+            type: "input",
+            name: "engName",
+            message: "What's the engineer's name?",
+          },
+          {
+            type: "input",
+            name: "engId",
+            message: "What's the engineer's employee ID?",
+          },
+          {
+            type: "input",
+            name: "engEmail",
+            message: "What's the engineer's email?",
+          },
+          {
+            type: "input",
+            name: "github",
+            message: "What's the engineer's GitHub username?",
+          },
+        ]);
+
+        // create a new Engineer instance and add it to the teamMembers array
+        const engineer = new Engineer(engName, engId, engEmail, github);
+        teamMembers.push(engineer);
+        break;
+
+      case "Intern":
+        const { internName, internId, internEmail, school } = await inquirer.prompt([
+          {
+            type: "input",
+            name: "internName",
+            message: "What's the intern's name?",
+          },
+          {
+            type: "input",
+            name: "internId",
+            message: "What's the intern's employee ID?",
+          },
+          {
+            type: "input",
+            name: "internEmail",
+            message: "What's the intern's email?",
+          },
+          {
+            type: "input",
+            name: "school",
+            message: "What's the intern's school called?",
+          },
+        ]);
+      
+          // create a new Intern instance and add it to the teamMembers array
+          const intern = new Intern(internName, internId, internEmail, school);
+          teamMembers.push(intern);
+          break;
+      };
+    };
+      const employees = [Engineer, Intern, Manager];
+      const html = render(employees);
+      
+      fs.writeFileSync(outputPath, html);
