63191a65e2329304d2e6a7634daabe1aef2a3fca
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-02-27 21:37:59 +0000

    fixed error in variables name

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+import Manager from './lib/Manager.js';
+import Engineer from './lib/Engineer.js';
+import Intern from './lib/Intern.js';
+import inquirer from 'inquirer';
+import path from 'path';
+import { promises as fs } from 'fs';
+import render from "./src/page-template.js";
+import Employee from "./lib/Employee.js";
+const __filename = fileURLToPath(import.meta.url);
+
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./src/page-template.js");
-
 const teamMembers = [];
 
 async function questions() {
@@ -44,14 +46,14 @@ async function questions() {
     let addMore = true;
 
     while (addMore) {
-        const { employeeType } = await inquirer.prompt({
+        const { Employee } = await inquirer.prompt({
             type: "list",
-            name: "employeeType",
+            name: "Employee",
             message: "Which type of employee would you like to add next?",
             choices: ["Engineer", "Intern", "I don't want to add any more team members"],
         });
 
-        switch (employeeType) {
+        switch (Employee) {
             case "Engineer":
                 const { engName, engId, engEmail, github } = await inquirer.prompt([
                     {
